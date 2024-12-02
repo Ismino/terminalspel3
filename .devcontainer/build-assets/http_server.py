bd6c0c7165f060404f6c6b0a231e0d55b189e835
Ismino <109960102+Ismino@users.noreply.github.com>
2024-12-02 07:48:49 -0800

    Initial commit

---
+++ .devcontainer/build-assets/http_server.py
@@ -0,0 +1,23 @@
+
+import http.server
+
+
+class NoCacheHTTPHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        """
+        Overrides default end_headers method
+        """
+        self.send_cache_headers()
+        http.server.SimpleHTTPRequestHandler.end_headers(self)
+
+    def send_cache_headers(self):
+        """
+        New method to send cache control headers
+        """
+        self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
+
+
+if __name__ == '__main__':
+    http.server.test(HandlerClass=NoCacheHTTPHandler)
